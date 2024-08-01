6c0e8e9b82b6beb37fa23d538bf34b8160b98799
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2024-08-01 13:07:46 +0100

    Session authentication with REST API

--- grvsaccount/urls.py
+++ grvsaccount/urls.py
@@ -1,11 +1,14 @@
 from django.urls import include, path
 from django.contrib.auth.views import logout_then_login
-from .views import ProfileView
+from . import views
 
 urlpatterns = [
+    path('api/login/', views.LoginView.as_view()),
+    path('api/logout/', views.LogoutView.as_view()),
     path('logout/', logout_then_login, name='logout_login'),
     path('logout/?next=<str:login_url>',
          logout_then_login),
-    path('profile/', ProfileView.as_view(), name='profile'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('apitest/', views.TemplateView.as_view(template_name='grvsaccount/login.html')),
     path('', include('django.contrib.auth.urls')),
 ]
--- grvsaccount/views.py
+++ grvsaccount/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
+from rest_framework.views import APIView, Response, status
 
 
 class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'registration/profile.html'
+
+
+class LoginView(APIView):
+    def post(self, request):
+        user = authenticate(
+            request,
+            username=request.data.get('username'),
+            password=request.data.get('password')
+        )
+        if user is None:
+            return Response({'detail': 'Incorrect username or password'}, status.HTTP_401_UNAUTHORIZED)
+        else:
+            login(request, user)
+            return Response(None, status.HTTP_204_NO_CONTENT)
+
+
+class LogoutView(APIView):
+    def post(self, request):
+        if request.user.is_authenticated:
+            logout(request)
+            return Response(None, status.HTTP_204_NO_CONTENT)
+        else:
+            return Response({'detail': 'Not logged in'}, status.HTTP_401_UNAUTHORIZED)
